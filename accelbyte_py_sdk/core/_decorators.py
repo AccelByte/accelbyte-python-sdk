44e16fdfab9d75b6cdd1a1d752c73663fcde12bf
Elmer Nocon <elmernocon@gmail.com>
2021-10-07 14:08:54 +0800

    feat(core): add deprecated decorator

--- accelbyte_py_sdk/core/_decorators.py
+++ accelbyte_py_sdk/core/_decorators.py
@@ -1,3 +1,19 @@
+import functools
+import warnings
+
+
+def deprecated(func):
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter("always", DeprecationWarning)
+        warnings.warn(f"Call to deprecated function '{func.__name__}'.",
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter("default", DeprecationWarning)
+        return func(*args, **kwargs)
+    return new_func
+
+
 def same_doc_as(source):
     def wrapper(target):
         target.__doc__ = source.__doc__
