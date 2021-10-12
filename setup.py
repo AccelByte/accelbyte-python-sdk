94133731e64192273e24285c94a134a65037a958
Elmer Nocon <elmernocon@gmail.com>
2021-10-12 20:48:15 +0800

    refactor(setup): refactor package setup

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+from pathlib import Path
+
+
+NAME = "accelbyte_py_sdk"
+VERSION = "0.2.0"
+AUTHOR = "AccelByte"
+AUTHOR_EMAIL = "dev@accelbyte.net"
+SHORT_DESCRIPTION = "AccelByte Python SDK"
+LONG_DESCRIPTION = (Path(__file__).parent / "README.md").read_text()
+LICENSE_TYPE = "MIT"
+PYTHON_VERSION = "3.9"
+
+config = {
+    "name": NAME,
+    "version": VERSION,
+
+    "author": AUTHOR,
+    "author_email": AUTHOR_EMAIL,
+
+    "description": SHORT_DESCRIPTION,
+    "long_description": LONG_DESCRIPTION,
+    "long_description_content_type": 'text/markdown',
+
+    "license": LICENSE_TYPE,
+    "license_files": ["LICENSE"],
+
+    "package_dir": {"": NAME},
+    "packages": setuptools.find_packages(where=NAME),
+
+    "python_requires": f">={PYTHON_VERSION}",
+    "classifiers": [
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        f"Programming Language :: Python :: {PYTHON_VERSION}",
+    ],
+}
+
+setuptools.setup(**config)
