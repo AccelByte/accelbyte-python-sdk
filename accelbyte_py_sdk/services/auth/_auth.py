5e3380e23a711bcc3a0641b349931d1f8e2b7997
Elmer Nocon <elmernocon@gmail.com>
2021-11-12 14:53:07 +0800

    feat(services): add async version of login_x methods

--- accelbyte_py_sdk/services/auth/_auth.py
+++ accelbyte_py_sdk/services/auth/_auth.py
@@ -8,9 +8,13 @@ from ...core import create_pkce_verifier_and_challenge_s256
 from ...core import create_basic_authentication
 
 from ...api.iam import authorize_v3
+from ...api.iam import authorize_v3_async
 from ...api.iam import user_authentication_v3
+from ...api.iam import user_authentication_v3_async
 from ...api.iam import token_grant_v3
+from ...api.iam import token_grant_v3_async
 from ...api.iam import token_revocation_v3
+from ...api.iam import token_revocation_v3_async
 
 
 def login_client(
@@ -23,6 +27,22 @@ def login_client(
         x_additional_headers["Authorization"] \
             = create_basic_authentication(client_id, client_secret)
     token, error = token_grant_v3("client_credentials")
+    if error:
+        return None, error
+
+    return token, None
+
+
+async def login_client_async(
+        client_id: Union[None, str] = None,
+        client_secret: Union[None, str] = None,
+        x_additional_headers: Union[None, Dict[str, str]] = None
+):
+    if client_id is not None and client_secret is not None:
+        x_additional_headers = x_additional_headers or {}
+        x_additional_headers["Authorization"] \
+            = create_basic_authentication(client_id, client_secret)
+    token, error = await token_grant_v3_async("client_credentials")
     if error:
         return None, error
 
@@ -81,6 +101,58 @@ def login_user(
     return token, None
 
 
+async def login_user_async(
+        username: str,
+        password: str,
+        scope: Union[None, str, List[str]] = None,
+        x_additional_headers: Union[None, Dict[str, str]] = None
+):
+    code_verifier, code_challenge, code_challenge_method \
+        = create_pkce_verifier_and_challenge_s256()
+
+    client_id, error = get_client_id()
+    if error:
+        return None, error
+
+    if scope is None:
+        scope = "account social"
+    elif isinstance(scope, List):
+        scope = " ".join(scope)
+
+    request_id, error = await authorize_v3_async(
+        response_type="code",
+        client_id=client_id,
+        scope=scope,
+        code_challenge=code_challenge,
+        code_challenge_method=code_challenge_method,
+        x_additional_headers=x_additional_headers
+    )
+    if error:
+        return None, error
+
+    code, error = await user_authentication_v3_async(
+        user_name=username,
+        password=password,
+        request_id=request_id,
+        client_id=client_id,
+        x_additional_headers=x_additional_headers
+    )
+    if error:
+        return None, error
+
+    token, error = await token_grant_v3_async(
+        grant_type="authorization_code",
+        code=code,
+        code_verifier=code_verifier,
+        redirect_uri="",
+        x_additional_headers=x_additional_headers
+    )
+    if error:
+        return None, error
+
+    return token, None
+
+
 def logout(x_additional_headers: Union[None, Dict[str, str]] = None):
     access_token, error = get_access_token()
     if error:
@@ -100,4 +172,24 @@ def logout(x_additional_headers: Union[None, Dict[str, str]] = None):
     return None, None
 
 
+async def logout_async(x_additional_headers: Union[None, Dict[str, str]] = None):
+    access_token, error = get_access_token()
+    if error:
+        return None, error
+
+    _, error = await token_revocation_v3_async(
+        token=access_token,
+        x_additional_headers=x_additional_headers
+    )
+    if error:
+        return None, error
+
+    _, error = remove_token()
+    if error:
+        return None, error
+
+    return None, None
+
+
 login = login_user
+login_async = login_user_async
