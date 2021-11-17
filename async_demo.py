47a2698f867a05c32387a852d0f229f9addc9655
Elmer Nocon <elmernocon@gmail.com>
2021-11-17 09:18:14 +0800

    docs(demo): updated demo scripts

--- async_demo.py
+++ async_demo.py
@@ -1,31 +1,48 @@
 import asyncio
-from os import environ
 
 import accelbyte_py_sdk
-from accelbyte_py_sdk.core import get_access_token
-from accelbyte_py_sdk.services.auth import login_async, logout_async
+from accelbyte_py_sdk.core import get_access_token, get_http_client
+from accelbyte_py_sdk.core import get_env_config, get_env_user_credentials, set_env_config, set_env_user_credentials
+from accelbyte_py_sdk.services.auth import login_client_async, login_user_async, logout_async
 
 
 async def main():
+    def print_error_and_exit(err):
+        print(err)
+        exit(1)
+
+    # (1.a) Initialize the SDK.
     accelbyte_py_sdk.initialize(
         options={
             "http": "HttpxHttpClient"
         }
     )
 
+    # (1.b) Verify that the HTTP client is async compatible.
+    http_client = get_http_client()
+    assert http_client.is_async_compatible()
+
+    # (2) Verify that we don't have any access tokens stored.
     _, error = get_access_token()
     assert error is not None
 
-    username = environ["AB_USERNAME"]
-    password = environ["AB_PASSWORD"]
+    # (3.a) Login.
+    username, password = get_env_user_credentials()
+    _, error = await login_user_async(username, password)
 
-    _, error = await login_async(username, password)
-    assert error is None
+    # (3.b) Verify that we have successfully logged in.
+    if error:
+        print_error_and_exit(error)
+
+    # (4) Get and print the stored access token.
     print(get_access_token())
 
+    # (5.a) Logout.
     _, error = await logout_async()
-    assert error is None
+    if error:
+        print_error_and_exit(error)
 
+    # (5.b) Verify that we don't have any access tokens stored.
     _, error = get_access_token()
     assert error is not None
 
--- demo.py
+++ demo.py
@@ -1,24 +1,54 @@
-from os import environ
-
 import accelbyte_py_sdk
+from accelbyte_py_sdk.core import MyConfigRepository
 from accelbyte_py_sdk.core import get_access_token
-from accelbyte_py_sdk.services.auth import login, logout
+from accelbyte_py_sdk.core import get_env_config, get_env_user_credentials, set_env_config, set_env_user_credentials
+from accelbyte_py_sdk.services.auth import login_client, login_user, logout
+
 
 if __name__ == "__main__":
-    accelbyte_py_sdk.initialize()
+    def print_error_and_exit(err):
+        print(err)
+        exit(1)
 
+    # (1) Initialize the SDK.
+    base_url, client_id, client_secret, namespace = get_env_config()
+    options = {
+        "config": MyConfigRepository(
+            base_url=base_url,
+            client_id=client_id,
+            client_secret=client_secret,
+            namespace=namespace
+        )
+    }
+    accelbyte_py_sdk.initialize(options=options)
+    # # :bulb: is actually the same as:
+    # accelbyte_py_sdk.initialize()
+
+    # (2) Verify that we don't have any access tokens stored.
     _, error = get_access_token()
     assert error is not None
 
-    username = environ["AB_USERNAME"]
-    password = environ["AB_PASSWORD"]
+    # (3.a1) Login using client credentials.
+    _, error = login_client(client_id, client_secret)
+    # # :bulb: is actually the same as:
+    # _, error = login_client()
 
-    _, error = login(username, password)
-    assert error is None
+    # # (3.a2) or Login using user credentials.
+    # username, password = get_env_user_credentials()
+    # _, error = login_user(username, password)
+
+    # (3.b) Verify that we have successfully logged in.
+    if error:
+        print_error_and_exit(error)
+
+    # (4) Get and print the stored access token.
     print(get_access_token())
 
+    # (5.a) Logout.
     _, error = logout()
-    assert error is None
+    if error:
+        print_error_and_exit(error)
 
+    # (5.b) Verify that we don't have any access tokens stored.
     _, error = get_access_token()
     assert error is not None
