84833e0793f5070c27e8a49fad470032680fedf7
Elmer Nocon <elmernocon@gmail.com>
2021-11-23 13:18:26 +0800

    feat(core): add create_full_url util func

--- accelbyte_py_sdk/core/_operation.py
+++ accelbyte_py_sdk/core/_operation.py
@@ -6,6 +6,37 @@ from ._utils import infer_headers_from_operation
 
 class Operation:
 
+    @staticmethod
+    def create_full_url(
+            url: str,
+            base_url: Union[None, str] = None,
+            path_params: Union[None, dict] = None,
+            query_params: Union[None, dict] = None
+    ) -> str:
+        # base url
+        if base_url:
+            if base_url.endswith("/"):
+                base_url = base_url.removesuffix("/")
+            result = base_url
+        else:
+            result = ""
+
+        # path params
+        if path_params:
+            for k, v in path_params.items():
+                url = url.replace(f"{{{k}}}", str(v))
+
+        result += url
+
+        # query params
+        if query_params:
+            flattened_query_params = []
+            for k, v in query_params.items():
+                flattened_query_params.append(f"{k}={v}")
+            result += "?" + "&".join(flattened_query_params)
+
+        return result
+
     def get_headers(self) -> Header:
         headers = Header()
         headers.update(self.get_header_params())
