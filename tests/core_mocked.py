90cb987f3c3cb56782368c798a3b0e67d57ff1c7
Elmer Nocon <elmernocon@gmail.com>
2022-01-06 17:39:47 +0800

    test(core): add test for handling file downloads

--- tests/core_mocked.py
+++ tests/core_mocked.py
@@ -26,5 +26,24 @@ def test_file_upload():
     accelbyte_py_sdk.reset()
 
 
+def test_file_download():
+    import accelbyte_py_sdk
+    from accelbyte_py_sdk.core import MyConfigRepository
+    from accelbyte_py_sdk.services.auth import login_client
+    from accelbyte_py_sdk.api.social import get_slot_data
+
+    config = MyConfigRepository(base_url=BASE_URL, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, namespace=NAMESPACE)
+    accelbyte_py_sdk.initialize(options={"config": config})
+
+    result, error = login_client()
+    assert error is None
+
+    result, error = get_slot_data("a", "a")
+    assert error is None
+
+    accelbyte_py_sdk.reset()
+
+
 if __name__ == "__main__":
+    test_file_download()
     test_file_upload()
