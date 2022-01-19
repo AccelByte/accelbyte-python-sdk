42d6354bfcc9d5780443046df5b14de74f13fe26
Elmer Nocon <elmernocon@gmail.com>
2022-01-19 08:34:33 +0800

    chore: bump version 0.5.0 → 0.6.0 2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 NAME = "accelbyte_py_sdk"
-VERSION = "0.5.0"
+VERSION = "0.6.0"
 AUTHOR = "AccelByte"
 AUTHOR_EMAIL = "dev@accelbyte.net"
 SHORT_DESCRIPTION = "AccelByte Python SDK"
