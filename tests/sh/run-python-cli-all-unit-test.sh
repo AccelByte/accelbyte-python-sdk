b55078c216aeb31433a72c64f8aada4b24a6e4e2
Elmer Nocon <elmernocon@gmail.com>
2022-02-15 12:11:39 +0800

    chore(sh unit tests): regenerate sh unit tests
    - update copyright notice

--- tests/sh/run-python-cli-all-unit-test.sh
+++ tests/sh/run-python-cli-all-unit-test.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+#Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+#This is licensed software from AccelByte Inc, for limitations
+#and restrictions contact your company contract manager.
+
 FILE_NAME='test-results.md'
 PIP='pip3'
 PYTHON='python3'
--- tests/sh/run-python-cli-matchmaking-unit-test.sh
+++ tests/sh/run-python-cli-matchmaking-unit-test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+#This is licensed software from AccelByte Inc, for limitations
+#and restrictions contact your company contract manager.
+
 #Meta:
 #- random seed: 256
 #- template file: python-cli-unit-test.j2
