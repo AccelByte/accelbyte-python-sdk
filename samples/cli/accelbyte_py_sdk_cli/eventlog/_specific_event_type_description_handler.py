8f3e1c1f53a67600b428440fa0c60c7e377f9ce8
Elmer Nocon <elmernocon@gmail.com>
2022-04-26 15:07:58 +0800

    chore(cli): regenerate cli commands
    - add code generated notice

--- samples/cli/accelbyte_py_sdk_cli/eventlog/_specific_event_type_description_handler.py
+++ samples/cli/accelbyte_py_sdk_cli/eventlog/_specific_event_type_description_handler.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
 # This is licensed software from AccelByte Inc, for limitations
 # and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
 
 # template_file: python-cli-command.j2
 
