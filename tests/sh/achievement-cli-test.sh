ba507eb5407b6c172530455d5cbfa5f1088fcdf1
Elmer Nocon <elmernocon@gmail.com>
2022-04-26 15:14:06 +0800

    chore(sh unit tests): regenerate unit tests
    - add code generated notice

--- tests/sh/achievement-cli-test.sh
+++ tests/sh/achievement-cli-test.sh
@@ -3,6 +3,8 @@
 # Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
 # This is licensed software from AccelByte Inc, for limitations
 # and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
 
 # Meta:
 # - random seed: 256
