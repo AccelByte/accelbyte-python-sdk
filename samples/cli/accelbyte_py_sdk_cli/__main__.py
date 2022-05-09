ff165fc92dc752757609389b60ae6ca54ede30c0
Elmer Nocon <elmernocon@gmail.com>
2022-05-09 11:41:17 +0800

    feat(cli): add formatters for start interactive session mode

--- samples/cli/accelbyte_py_sdk_cli/__main__.py
+++ samples/cli/accelbyte_py_sdk_cli/__main__.py
@@ -1,15 +1,10 @@
-import asyncio
 import logging
 import shlex
-from typing import Any, IO, Optional
+from typing import Any, Optional
 
 import click
-import websockets
-from aioconsole import ainput
 
 from accelbyte_py_sdk import initialize
-from accelbyte_py_sdk.core import get_base_url
-from accelbyte_py_sdk.core import WebsocketsWSClient
 
 from accelbyte_py_sdk_cli.iam import commands as iam_commands
 from accelbyte_py_sdk_cli.achievement import commands as achievement_commands
@@ -32,205 +27,43 @@ from accelbyte_py_sdk_cli.sessionbrowser import commands as sessionbrowser_comma
 from accelbyte_py_sdk_cli.social import commands as social_commands
 from accelbyte_py_sdk_cli.ugc import commands as ugc_commands
 
+from accelbyte_py_sdk_cli._sessions import SessionWriter
+from accelbyte_py_sdk_cli._sessions import TapSessionWriter
+from accelbyte_py_sdk_cli._sessions import one_shot_websocket
+from accelbyte_py_sdk_cli._sessions import start_batched_ws_session
+
 
 @click.group()
 def entry_point():
     initialize()
 
 
-async def one_shot_websocket_async(
-        raw_text: str,
-        mode: Optional[str] = None,
-        timeout: Optional[float] = None,
-        uri_prefix: Optional[str] = None,
-):
-    mode = mode or "echo"
-    sleep_interval = 0.03
-    timeout = timeout or 10.0
-    uri_prefix = uri_prefix or "ws://"
-
-    base_url, error = get_base_url()
-    if error:
-        exit(1)
-
-    elapsed: float = 0.0
-    raw_request: str = raw_text.replace("\\n", "\n")
-    raw_response: Optional[str] = None
-
-    async def on_message(msg: str):
-        nonlocal raw_response
-        raw_response = msg
-        click.echo(f"[RECV]: {msg}")
-
-    ws_client = WebsocketsWSClient(
-        uri=base_url,
-        uri_prefix=uri_prefix
-    )
-    ws_client.listeners.append(on_message)
-
-    click.echo(f"[INFO]: connecting to {ws_client.uri}")
-    await ws_client.connect()
-    click.echo("[INFO]: connected")
-
-    await ws_client.send(raw_request)
-    click.echo(f"[SEND]: {raw_request}")
-
-    while elapsed < timeout and raw_response is None:
-        await asyncio.sleep(sleep_interval)
-        elapsed += sleep_interval
-
-    click.echo("[INFO]: disconnecting")
-    await ws_client.disconnect()
-    click.echo("[INFO]: disconnected")
-
-    if elapsed >= timeout:
-        raise TimeoutError
-    elif raw_response is None:
-        raise Exception("NoResponse")
-
-    if mode == "echo":
-        if raw_request != raw_response:
-            flat_request = "\\n".join(raw_request.splitlines(keepends=False))
-            flat_response = "\\n".join(raw_response.splitlines(keepends=False))
-            raise Exception(f"'{flat_request}' != '{flat_response}'")
-    else:
-        raise NotImplementedError
-
-
-@click.command()
-@click.argument("raw_text", type=str)
-@click.option("--mode", type=click.Choice(["echo"], case_sensitive=False))
-@click.option("--timeout", type=float)
-@click.option("--uri_prefix", type=str)
-def one_shot_websocket(
-        raw_text: str,
-        mode: Optional[str] = None,
-        timeout: Optional[float] = None,
-        uri_prefix: Optional[str] = None,
-):
-    asyncio.run(
-        one_shot_websocket_async(
-            raw_text=raw_text,
-            mode=mode,
-            timeout=timeout,
-            uri_prefix=uri_prefix,
-        )
-    )
-
-
-@click.command()
-@click.option("--compare_with_last_message/--no_compare_with_last_message", default=False)
-@click.option("--exit_on_error/--no_exit_on_error", default=bool)
-@click.option("--uri_prefix", type=str)
-def enter_websocket_mode(
-        compare_with_last_message: bool,
-        exit_on_error: bool,
-        uri_prefix: Optional[str] = None
-):
-    asyncio.run(websocket_mode(
-        compare_with_last_message=compare_with_last_message,
-        exit_on_error=exit_on_error,
-        uri_prefix=uri_prefix
-    ))
-
-
-async def websocket_mode(
-        compare_with_last_message: Optional[bool] = None,
-        exit_on_error: Optional[bool] = None,
-        uri_prefix: Optional[str] = None
-):
-    compare_with_last_message = compare_with_last_message or False
-    exit_on_error = exit_on_error or False
-    uri_prefix = uri_prefix or "ws://"
-
-    websocket_mode.compare_with_last_message = compare_with_last_message
-    websocket_mode.exit_on_error = exit_on_error
-
-    base_url, error = get_base_url()
-    if error:
-        exit(1)
-
-    ws_client = WebsocketsWSClient(uri=base_url, uri_prefix=uri_prefix)
-    ws_client.listeners.append(websocket_listener)
-
-    click.echo(f"[INFO]: connecting to {ws_client.uri}")
-    await ws_client.connect()
-    click.echo("[INFO]: connected")
-
-    while True:
-        await wait_for_response()
-        if websocket_mode.should_quit:
-            break
-        inp, src = await read_line_from_stdin()
-        if inp:
-            if inp == ":q":
-                break
-            if inp is not None:
-                await ws_client.send(inp)
-                websocket_mode.is_waiting_for_response = True
-                websocket_mode.last_message = inp
-                click.echo(f"[SEND]: {inp}")
-
-    click.echo("[INFO]: disconnecting")
-    await ws_client.disconnect()
-    click.echo("[INFO]: disconnected")
-
-    if websocket_mode.error is not None:
-        raise Exception(f"WebSocket Mode Error: {str(websocket_mode.error)}")
-
-websocket_mode.compare_with_last_message = False
-websocket_mode.error = None
-websocket_mode.exit_on_error = False
-websocket_mode.is_waiting_for_response = False
-websocket_mode.last_message = None
-websocket_mode.should_quit = False
-
-
-async def read_line_from_stdin():
-    result = await ainput()
-    result = str(result)
-    result = result.replace("\\n", "\n")
-    return result, "stdin"
-
-
-async def wait_for_response():
-    sleep_duration = 0.016
-    while websocket_mode.is_waiting_for_response:
-        await asyncio.sleep(sleep_duration)
-
-
-async def websocket_listener(message: str):
-    websocket_mode.is_waiting_for_response = False
-    if websocket_mode.compare_with_last_message and websocket_mode.last_message != message:
-        flattened_message = "\\n".join(message.splitlines(keepends=False))
-        error = f"'{websocket_mode.last_message}' != '{flattened_message}'"
-        click.echo(f"[ERRO]: {error}")
-        if websocket_mode.exit_on_error:
-            websocket_mode.error = error
-            websocket_mode.should_quit = True
-        return
-    click.echo(f"[RECV]: {message}")
-
-
+# noinspection PyUnresolvedReferences
 @click.command()
 @click.option("--continue_on_error", is_flag=True)
-@click.option("--print_success", is_flag=True)
 @click.option("--verbose", is_flag=True)
+@click.option("--writer", type=click.Choice(['default', 'tap'], case_sensitive=False))
 @click.pass_context
 def start_interactive_session(
         ctx,
         continue_on_error: bool = False,
-        print_success: bool = False,
-        verbose: bool = False
+        verbose: bool = False,
+        writer: str = "default",
 ):
-    click.echo = echo_intercept(ctx)
-    logging.getLogger("accelbyte_py_sdk.http").error = log_intercept(ctx)
+    writer = TapSessionWriter() if writer == "tap" else SessionWriter()
+
+    click.echo = __intercept(ctx, "echo")
+    logging.getLogger("accelbyte_py_sdk.http").error = __intercept(ctx, "log")
+
+    error_count = 0
 
     while True:
         message = input()
 
         if message == "exit()":
             break
+        elif message.strip() == "":
+            continue
 
         # noinspection PyBroadException
         # pylint: disable=broad-except
@@ -239,8 +72,32 @@ def start_interactive_session(
             cmd_name, cmd, args = entry_point.resolve_command(ctx, args)
             sub_ctx = cmd.make_context(cmd_name, list(args), parent=ctx)
         except Exception as e:
-            print(f"error: unable to resolve command")
+            writer.write_warning(f"unable to resolve command '{message}'")
             if continue_on_error:
+                error_count += 1
+                continue
+            else:
+                exit(1)
+                return
+
+        # noinspection PyBroadException
+        # pylint: disable=broad-except
+        try:
+            operation_id = cmd.operation_id
+            is_deprecated = cmd.is_deprecated
+        except Exception as e:
+            writer.write_warning(f"operation identifier not found '{message}'")
+            if continue_on_error:
+                error_count += 1
+                continue
+            else:
+                exit(1)
+                return
+
+        if is_deprecated:
+            writer.write_warning(f"deprecated {operation_id}")
+            if continue_on_error:
+                error_count += 1
                 continue
             else:
                 exit(1)
@@ -251,128 +108,51 @@ def start_interactive_session(
         try:
             sub_ctx.command.invoke(sub_ctx)
         except Exception as e:
-            error = str(e)
-            if ctx.log:
-                error += "\n" + ctx.log
+            indent = "  "
+            delimiter = f"\n{indent}"
+            error = operation_id
+            lines = ["---", str(e)]
+            if hasattr(ctx, "log") and isinstance(ctx.log, str):
+                lines.extend(ctx.log.splitlines(keepends=False))
                 ctx.log = None
-            print(f"error: {error}")
+            error += delimiter + delimiter.join(lines)
+            writer.write_error(error.rstrip())
             if continue_on_error:
+                error_count += 1
                 continue
             else:
                 exit(1)
                 return
 
-        if print_success:
-            if verbose and ctx.echo:
-                print(f"success:\n{str(ctx.echo)}")
-                ctx.echo = None
-            else:
-                print("success")
+        success = operation_id
+        if verbose and hasattr(ctx, "echo") and ctx.echo:
+            echo = str(ctx.echo)
+            if echo:
+                indent = "  "
+                success += f"\n{indent}---\n"
+                success += "".join(f"{indent}{line}" for line in echo.splitlines(keepends=True)).rstrip()
+            ctx.echo = None
+        writer.write_log(success)
 
+    print(writer.flush())
 
-@click.command()
-@click.option("--continue_on_error", is_flag=True)
-@click.option("--print_success", is_flag=True)
-@click.option("--verbose", is_flag=True)
-@click.pass_context
-def start_interactive_ws_session(
-        ctx,
-        continue_on_error: bool = False,
-        print_success: bool = False,
-        verbose: bool = False
-):
-    options = {
-        "continue_on_error": continue_on_error,
-        "print_success": print_success,
-        "verbose": verbose,
-    }
-    asyncio.run(start_interactive_ws_session_async(ctx, options))
-
-
-def echo_intercept(ctx):
-    def echo_intercept_internal(
-        message: Optional[Any] = None,
-        file: Optional[IO] = None,
-        nl: bool = True,
-        err: bool = False,
-        color: Optional[bool] = None,
-    ) -> None:
-        ctx.echo = message
-    return echo_intercept_internal
-
-
-def log_intercept(ctx):
-    def log_intercept_internal(
-        message: Optional[Any] = None,
-        *args,
-        **kwargs
-    ) -> None:
-        ctx.log = message
-    return log_intercept_internal
-
-
-def process_cmd(ctx, options):
-    click.echo = echo_intercept(ctx)
-    logging.getLogger("accelbyte_py_sdk.http").error = log_intercept(ctx)
-
-    verbose = options.get("verbose", False)
-    print_success = options.get("print_success", False)
-    continue_on_error = options.get("continue_on_error", False)
-
-    async def process_cmd_async(websocket, path):
-        async for message in websocket:
-            if message == "exit()":
-                await websocket.close()
-                break
-
-            # noinspection PyBroadException
-            # pylint: disable=broad-except
-            try:
-                args = shlex.split(message)
-                cmd_name, cmd, args = entry_point.resolve_command(ctx, args)
-                sub_ctx = cmd.make_context(cmd_name, list(args), parent=ctx)
-            except Exception as e:
-                await websocket.send("error: unable to resolve command")
-                if continue_on_error:
-                    continue
-                else:
-                    await websocket.close()
-                    break
-
-            # noinspection PyBroadException
-            # pylint: disable=broad-except
-            try:
-                sub_ctx.command.invoke(sub_ctx)
-            except Exception as e:
-                error = str(e)
-                if ctx.log:
-                    error += "\n" + ctx.log
-                    ctx.log = None
-                await websocket.send(f"error: {error}")
-                if continue_on_error:
-                    continue
-                else:
-                    await websocket.close()
-                    break
-
-            if print_success:
-                if verbose and ctx.echo:
-                    await websocket.send(f"success:\n{str(ctx.echo)}")
-                    ctx.echo = None
-                else:
-                    await websocket.send("success")
-
-    return process_cmd_async
-
-
-async def start_interactive_ws_session_async(ctx, options: dict):
-    async with websockets.serve(process_cmd(ctx, options), "localhost", 8765):
-        await asyncio.Future()
+    if error_count != 0:
+        exit(1)
 
 
 def add_commands(grp, cmds, prefix: str = None):
     for cmd in cmds:
         grp.add_command(cmd, name=f"{f'{prefix}-' if prefix is not None else ''}{cmd.name}")
+
+
+def __intercept(obj, attr: str):
+    def __intercept_impl(
+            message: Optional[Any] = None,
+            *args,
+            **kwargs
+    ):
+        setattr(obj, "attr", message)
+    return __intercept_impl
 
 
 add_commands(entry_point, iam_commands, prefix="iam")
@@ -396,10 +176,9 @@ add_commands(entry_point, sessionbrowser_commands, prefix="sessionbrowser")
 add_commands(entry_point, social_commands, prefix="social")
 add_commands(entry_point, ugc_commands, prefix="ugc")
 
-entry_point.add_command(enter_websocket_mode)
-entry_point.add_command(one_shot_websocket)
 entry_point.add_command(start_interactive_session)
-entry_point.add_command(start_interactive_ws_session)
+entry_point.add_command(one_shot_websocket)
+entry_point.add_command(start_batched_ws_session)
 
 
 if __name__ == "__main__":
---
+++ samples/cli/accelbyte_py_sdk_cli/_sessions.py
@@ -0,0 +1,210 @@
+import asyncio
+import logging
+import shlex
+from typing import Any, IO, List, Optional
+
+import click
+
+from accelbyte_py_sdk.core import get_base_url
+from accelbyte_py_sdk.core import WebsocketsWSClient
+
+
+class SessionWriter:
+
+    def __init__(self):
+        self.writes = []
+
+    def write_log(self, log: str) -> str:
+        log = f"success: {log}"
+        self.writes.append(log)
+        return log
+
+    def write_error(self, error: str) -> str:
+        error = f"error: {error}"
+        self.writes.append(error)
+        return error
+
+    def write_warning(self, warning: str) -> str:
+        warning = f"warning: {warning}"
+        self.writes.append(warning)
+        return warning
+
+    def flush(self) -> str:
+        return "\n".join(self.writes)
+
+
+class TapSessionWriter(SessionWriter):
+
+    def __init__(self):
+        super().__init__()
+        self.count = 0
+
+    def write_log(self, log: str) -> str:
+        self.count += 1
+        log = f"ok {self.count} - {log}"
+        self.writes.append(log)
+        return log
+
+    def write_error(self, error: str) -> str:
+        self.count += 1
+        error = f"not ok {self.count} - {error}"
+        self.writes.append(error)
+        return error
+
+    def write_warning(self, warning: str) -> str:
+        self.count += 1
+        warning = f"ok {self.count} - # SKIP {warning}"
+        self.writes.append(warning)
+        return warning
+
+    def flush(self) -> str:
+        return f"TAP version 13\n1..{self.count}\n" + "\n".join(self.writes)
+
+
+async def _start_batched_ws_session_async(
+        messages: List[str],
+        continue_on_error: bool,
+        timeout: float,
+        mode: str,
+        uri_prefix: str,
+        writer: str,
+):
+    writer = TapSessionWriter() if writer == "tap" else SessionWriter()
+
+    if mode != "echo":
+        writer.write_error("ModeNotSupportedError")
+        print(writer.flush())
+        exit(1)
+
+    base_url, error = get_base_url()
+    if error:
+        writer.write_error("BaseUrlNotFoundError")
+        print(writer.flush())
+        exit(1)
+
+    raw_response: Optional[str] = None
+
+    async def on_message_(msg: str):
+        nonlocal raw_response
+        raw_response = msg
+
+    ws_client = WebsocketsWSClient(
+        uri=base_url,
+        uri_prefix=uri_prefix,
+    )
+    ws_client.listeners.append(on_message_)
+
+    try:
+        await ws_client.connect()
+
+        error_count = 0
+        for message in messages:
+            error = None
+            elapsed = 0.0
+            sleep = 0.03
+            raw_request = message.replace("\\n", "\n")
+            raw_response = None
+            await ws_client.send(raw_request)
+            while elapsed < timeout and raw_response is None:
+                await asyncio.sleep(sleep)
+                elapsed += sleep
+            if elapsed >= timeout:
+                error = "TimeoutError"
+            elif raw_response is None:
+                error = "NoResponseError"
+            if raw_request != raw_response:
+                flat_request = "\\n".join(raw_request.splitlines(keepends=False))
+                flat_response = "\\n".join(raw_response.splitlines(keepends=False))
+                error = f"'{flat_request}' != '{flat_response}'"
+            if error is not None:
+                error_count += 1
+                writer.write_error(error)
+                if not continue_on_error:
+                    break
+            else:
+                writer.write_log(raw_response.splitlines(keepends=False)[0].removeprefix("type: "))
+    finally:
+        await ws_client.disconnect()
+        print(writer.flush())
+
+    exit(0 if error_count == 0 else 1)
+
+
+def _start_batched_ws_session(
+        continue_on_error: bool = False,
+        mode: str = "echo",
+        timeout: float = 10.0,
+        uri_prefix: str = "ws://",
+        writer: str = "default",
+):
+    messages: List[str] = []
+
+    while True:
+        message = input()
+
+        if message == "send()":
+            break
+        elif message.strip() == "":
+            continue
+
+        message = message.strip("'")
+
+        messages.append(message)
+
+    asyncio.run(
+        _start_batched_ws_session_async(
+            messages=messages,
+            continue_on_error=continue_on_error,
+            mode=mode,
+            timeout=timeout,
+            uri_prefix=uri_prefix,
+            writer=writer
+        )
+    )
+
+
+start_batched_ws_session = click.command("start-batched-ws-session")(
+    click.option("--continue_on_error", is_flag=True)(
+        click.option("--mode", type=click.Choice(["echo"], case_sensitive=False), default="echo")(
+            click.option("--timeout", type=float, default=10.0)(
+                click.option("--uri_prefix", type=str, default="ws://")(
+                    click.option("--writer", type=click.Choice(["default", "tap"], case_sensitive=False), default="default")
+                        (_start_batched_ws_session))))))
+
+
+def __intercept(ctx, attr: str):
+    def __intercept_impl(
+            message: Optional[Any] = None,
+            *args,
+            **kwargs
+    ):
+        setattr(ctx, "attr", message)
+    return __intercept_impl
+
+
+def _one_shot_websocket(
+        raw_text: str,
+        mode: str = "echo",
+        timeout: float = 10.0,
+        uri_prefix: str = "ws://",
+        writer: str = "default",
+):
+    asyncio.run(
+        _start_batched_ws_session_async(
+            messages=[raw_text],
+            continue_on_error=False,
+            mode=mode,
+            timeout=timeout,
+            uri_prefix=uri_prefix,
+            writer=writer
+        )
+    )
+
+
+one_shot_websocket = click.command("one-shot-websocket")(
+    click.argument("raw_text", type=str)(
+        click.option("--mode", type=click.Choice(["echo"], case_sensitive=False), default="echo")(
+            click.option("--timeout", type=float, default=10.0)(
+                click.option("--uri_prefix", type=str, default="ws://")(
+                    click.option("--writer", type=click.Choice(["default", "tap"], case_sensitive=False), default="default")
+                        (_one_shot_websocket))))))
