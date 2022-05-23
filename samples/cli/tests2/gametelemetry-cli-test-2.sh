a2a1d6e8f03763972e1bbbe56d03db6ee3202462
Elmer Nocon <elmernocon@gmail.com>
2022-05-23 16:16:14 +0800

    chore(sh unit tests): regenerate unit tests

--- samples/cli/tests2/gametelemetry-cli-test-2.sh
+++ samples/cli/tests2/gametelemetry-cli-test-2.sh
@@ -29,8 +29,6 @@ touch "tmp.dat"
 export PYTHONPATH=$MODULE_PATH:$PYTHONPATH
 
 $PYTHON -m $MODULE 'start-interactive-session' --continue_on_error '--writer=tap' << END
-gametelemetry-admin-get-events-game-telemetry-v1-admin-events-get --login_with_auth "Bearer foo"
-gametelemetry-admin-get-namespace-game-telemetry-v1-admin-telemetrynamespace-get --login_with_auth "Bearer foo"
 gametelemetry-protected-save-events-game-telemetry-v1-protected-events-post '[{"EventId": "FtBxyZcD", "EventName": "XBpGlsQu", "EventNamespace": "Ju8vMf0I", "EventTimestamp": "1980-10-10T00:00:00Z", "Payload": {"kTrd8IDc": {}}}]' --login_with_auth "Bearer foo"
 gametelemetry-protected-get-playtime-game-telemetry-v1-protected-steam-ids-steam-id-playtime-get 'V2zXnTKj' --login_with_auth "Bearer foo"
 gametelemetry-protected-update-playtime-game-telemetry-v1-protected-steam-ids-steam-id-playtime-playtime-put 'XY1bPqam' 'iBxx9Cs1' --login_with_auth "Bearer foo"
