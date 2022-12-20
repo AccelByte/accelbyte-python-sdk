3b3b529174f4127580230ea1fd68222360de6bf3
Elmer Nocon <elmernocon@gmail.com>
2022-12-20 08:52:48 +0800

    fix(integration tests): add missing tests

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,7 +1,13 @@
 from .docs import DocsTestCase
 
+from .api.achievement import AchievementTestCase
 from .api.basic import BasicTestCase
+from .api.cloudsave import CloudSaveTestCase
 from .api.dslogmanager import DSLogManagerTestCase
+from .api.dsmc import DSMCTestCase
+# TODO: from .api.eventlog import EventLogTestCase
+from .api.gametelemetry import GametelemetryTestCase
+from .api.gdpr import GDPRTestCase
 from .api.group import GroupTestCase
 from .api.iam import IAMTestCase
 from .api.leaderboard import LeaderboardTestCase
@@ -9,9 +15,12 @@ from .api.legal import LegalTestCase
 from .api.lobby import AsyncLobbyTestCase, LobbyTestCase
 from .api.match2 import Match2TestCase
 from .api.matchmaking import MatchmakingTestCase
+from .api.platform import PlatformTestCase
 from .api.qosm import QosmTestCase
 from .api.seasonpass import SeasonPassTestCase
+from .api.session import SessionTestCase
 from .api.sessionbrowser import SessionBrowserTestCase
 from .api.social import SocialTestCase
+from .api.ugc import UGCTestCase
 
 from .services.auth import AuthServicesTestCase
