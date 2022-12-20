9950d0eb67cf30379dc3e39225a09da1d99bb815
Elmer Nocon <elmernocon@gmail.com>
2022-12-20 09:49:14 +0800

    chore(integration tests): temporarily disable event log tests

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -5,7 +5,7 @@ from .api.basic import BasicTestCase
 from .api.cloudsave import CloudSaveTestCase
 from .api.dslogmanager import DSLogManagerTestCase
 from .api.dsmc import DSMCTestCase
-from .api.eventlog import EventLogTestCase
+# from .api.eventlog import EventLogTestCase
 from .api.gametelemetry import GametelemetryTestCase
 from .api.gdpr import GDPRTestCase
 from .api.group import GroupTestCase
