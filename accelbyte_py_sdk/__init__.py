74ce6a63a17d0eae7b390d7e04547bca85514220
Elmer Nocon <elmernocon@gmail.com>
2023-01-03 10:15:40 +0800

    chore: bump version 0.30.0 → 0.31.0

--- accelbyte_py_sdk/__init__.py
+++ accelbyte_py_sdk/__init__.py
@@ -5,7 +5,7 @@
 """The top-level package for the AccelByte Python SDK."""
 
 __product__ = "AccelBytePythonSDK"
-__version__ = "0.30.0"
+__version__ = "0.31.0"
 __author__ = "AccelByte"
 __email__ = "dev@accelbyte.net"
 
