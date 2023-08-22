a9643386564a605c4a6ab37918da0cc498697d60
Elmer Nocon <elmernocon@gmail.com>
2023-08-22 09:07:40 +0800

    test(jwt): add tests for parse_access_token

--- tests/integration/token_validator/token_validator.py
+++ tests/integration/token_validator/token_validator.py
@@ -5,6 +5,7 @@ import accelbyte_py_sdk.services.auth as auth_service
 
 from accelbyte_py_sdk import AccelByteSDK
 from accelbyte_py_sdk.core import SDK
+from accelbyte_py_sdk.services.auth import parse_access_token
 from accelbyte_py_sdk.token_validation.caching import CachingTokenValidator
 
 from tests.integration.test_case import IntegrationTestCase
@@ -93,5 +94,99 @@ class TokenValidatorTestCase(IntegrationTestCase):
         self.assertIsNotNone(error, error)
 
     # endregion test:validate_token
+
+    # region test:parse_access_token
+
+    def test_parse_access_token(self):
+        # arrange
+        generate_user_result, error = self.generate_user()
+        if error:
+            self.skipTest(reason=f"unable to create user: {error}")
+        username, password, user_id = generate_user_result
+
+        self.user_id = user_id
+
+        user_sdk, error = self.create_user_sdk(
+            username=username,
+            password=password,
+            existing_sdk=SDK,
+        )
+        if error:
+            self.skipTest(reason=f"unable to create user sdk: {error}")
+        else:
+            self.sdks.append(user_sdk)
+
+        access_token = user_sdk.get_token_repository().get_access_token()
+        if access_token is None:
+            self.skipTest(reason=f"unable to get access token")
+
+        claims, error = parse_access_token(access_token)
+        self.assertIsNone(error)
+
+    # endregion test:parse_access_token
+
+    # region test:parse_and_iam_validate_access_token
+
+    def test_parse_and_iam_validate_access_token(self):
+        # arrange
+        generate_user_result, error = self.generate_user()
+        if error:
+            self.skipTest(reason=f"unable to create user: {error}")
+        username, password, user_id = generate_user_result
+
+        self.user_id = user_id
+
+        user_sdk, error = self.create_user_sdk(
+            username=username,
+            password=password,
+            existing_sdk=SDK,
+        )
+        if error:
+            self.skipTest(reason=f"unable to create user sdk: {error}")
+        else:
+            self.sdks.append(user_sdk)
+
+        access_token = user_sdk.get_token_repository().get_access_token()
+        if access_token is None:
+            self.skipTest(reason=f"unable to get access token")
+
+        claims, error = parse_access_token(access_token, validator="iam")
+        self.assertIsNone(error, error)
+
+    # endregion test:parse_and_iam_validate_access_token
+
+    # region test:test_parse_and_local_validate_access_token
+
+    def test_parse_and_local_validate_access_token(self):
+        # arrange
+        generate_user_result, error = self.generate_user()
+        if error:
+            self.skipTest(reason=f"unable to create user: {error}")
+        username, password, user_id = generate_user_result
+
+        self.user_id = user_id
+
+        user_sdk, error = self.create_user_sdk(
+            username=username,
+            password=password,
+            existing_sdk=SDK,
+        )
+        if error:
+            self.skipTest(reason=f"unable to create user sdk: {error}")
+        else:
+            self.sdks.append(user_sdk)
+
+        access_token = user_sdk.get_token_repository().get_access_token()
+        if access_token is None:
+            self.skipTest(reason=f"unable to get access token")
+
+        token_validator = CachingTokenValidator(sdk=SDK)
+
+        time.sleep(3)  # wait for the token validator to do some caching first
+
+        claims, error = parse_access_token(access_token, validator=token_validator)
+        self.assertIsNone(error, error)
+
+    # endregion test:test_parse_and_local_validate_access_token
 
     # end of file
