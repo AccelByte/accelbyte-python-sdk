334b53f28a7f303a9cc409309ddd767f9c065cc7
Elmer Nocon <elmernocon@gmail.com>
2024-02-01 22:23:50 +0800

    test(integration): add dsartifact integration tests

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -6,6 +6,7 @@ from .api.basic import BasicTestCase
 from .api.chat import ChatTestCase
 from .api.cloudsave import CloudSaveTestCase
 
+# from .api.dsartifact import DSArtifactTestCase
 # from .api.dslogmanager import DSLogManagerTestCase
 # from .api.dsmc import DSMCTestCase, DSMCSessionTestCase
 from .api.eventlog import EventLogTestCase
