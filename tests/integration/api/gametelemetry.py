4e2ba127e267797131f2d33e4fff6ebe86ccd9b0
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2025-03-19 10:58:14 +0700

    test(integration): gametelemetry - remove deprecated endpoints

--- tests/integration/api/gametelemetry.py
+++ tests/integration/api/gametelemetry.py
@@ -2,35 +2,6 @@ from tests.integration.test_case import IntegrationTestCase
 
 
 class GametelemetryTestCase(IntegrationTestCase):
-    steam_id: str = "76561199259217491"
-
-    # region test:get_playtime_v1
-
-    def test_get_playtime_v1(self):
-        if self.using_ags_starter:
-            self.skipTest(reason="Test not applicable to AGS Starter.")
-
-        from accelbyte_py_sdk.api.gametelemetry import (
-            protected_get_playtime_game_telemetry_v1_protected_steam_ids_steam_id_playtime_get,
-        )
-
-        # arrange
-
-        # act
-        (
-            result,
-            error,
-        ) = protected_get_playtime_game_telemetry_v1_protected_steam_ids_steam_id_playtime_get(
-            steam_id=self.steam_id
-        )
-
-        # assert
-        self.assertIsNone(error)
-
-        if not isinstance(result, int):
-            self.skipTest(reason="Playtime not of type int.")
-
-    # endregion test:get_playtime_v1
 
     # region test:save_events_v1
 
@@ -61,37 +32,3 @@ class GametelemetryTestCase(IntegrationTestCase):
         self.assertIsNone(error)
 
     # endregion test:save_events_v1
-
-    # region test:update_playtime_v1
-
-    def test_update_playtime_v1(self):
-        if self.using_ags_starter:
-            self.skipTest(reason="Test not applicable to AGS Starter.")
-
-        from accelbyte_py_sdk.api.gametelemetry import (
-            protected_update_playtime_game_telemetry_v1_protected_steam_ids_steam_id_playtime_playtime_put,
-        )
-        from accelbyte_py_sdk.api.gametelemetry.models import BaseErrorResponse
-
-        # act
-        (
-            result,
-            error,
-        ) = protected_update_playtime_game_telemetry_v1_protected_steam_ids_steam_id_playtime_playtime_put(
-            playtime="4",
-            steam_id=self.steam_id,
-        )
-
-        # assert
-        if (
-            error is not None
-            and isinstance(error, BaseErrorResponse)
-            and "user not found" in str(error.error_message).lower()
-        ):
-            self.skipTest(reason="User was not found.")
-        else:
-            self.assertIsNone(error, error)
-
-    # endregion test:update_playtime_v1
-
-    # end of file
