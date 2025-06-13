fee24c14b3f85e82c79b79dddd9c3f679ca5a00a
Elmer Nocon <elmernocon@gmail.com>
2025-06-13 08:30:24 +0800

    feat(core): add get_version

--- accelbyte_py_sdk/__init__.py
+++ accelbyte_py_sdk/__init__.py
@@ -14,3 +14,27 @@ from .core import AccelByteSDK
 from .core import is_initialized
 from .core import initialize
 from .core import reset
+
+
+def get_version(
+    latest: bool = False,
+    full: bool = False,
+    **kwargs,
+) -> str:
+    result = f"{__product__} {__version__}" if full else __version__
+
+    if latest:
+        # noinspection PyBroadException
+        try:
+            import requests
+
+            response = requests.get(url=f"https://pypi.org/pypi/accelbyte-py-sdk/json")
+            if response.ok:
+                response_json = response.json()
+                latest_version = response_json.get("info", {}).get("version")
+                if latest_version is not None and __version__ != latest_version:
+                    result += f" (latest: {latest_version})"
+        except Exception:
+            pass  # Best-effort version check: ignore all errors silently.
+
+    return result
